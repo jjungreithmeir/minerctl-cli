e0b21527db5818850650c4eaafeeb396c1e6b422
Jakob Jungreithmeir <jakob.jungreithmeir@bittex.at>
2018-06-27 16:44:06 +0200

    add more GET requests

--- cli.py
+++ cli.py
@@ -37,6 +37,8 @@ def main():
                         dest='info', action='store_true')
     parser.add_argument('-k', '--key', help='set private key location',
                         dest='key')
+    parser.add_argument('-a', '--all', help='show all available data',
+                        default=False, dest='all', action='store_true')
     parser.add_argument('-t', '--temp', help='show temperatures',
                         default=False, dest='temp', action='store_true')
     parser.add_argument('-f', '--filter', help='show filter status and data',
@@ -59,21 +61,22 @@ def main():
     else:
         key_location = _load_config()
 
+    # TODO make the address setable (also port) and add the http part if it is not present
     sec_handler = SecureHandler(key_location, 'http://127.0.0.1:12345')
 
-    if args.info:
-        pass
-    if args.temp:
+    if args.info or args.all:
+        print(sec_handler.get('/info'))
+    if args.temp or args.all:
         print(sec_handler.get('/temp'))
-    if args.filter:
-        pass
-    if args.ventilation:
-        pass
-    if args.operation:
-        pass
-    if args.pid:
-        pass
-    if args.miner:
+    if args.filter or args.all:
+        print(sec_handler.get('/filter'))
+    if args.ventilation or args.all:
+        print(sec_handler.get('/fans'))
+    if args.operation or args.all:
+        print(sec_handler.get('/mode'))
+    if args.pid or args.all:
+        print(sec_handler.get('/pid'))
+    if args.miner or args.all:
         pass
     if len(sys.argv) == 1:
         parser.print_help()
