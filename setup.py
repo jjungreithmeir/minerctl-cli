79cfe00dc3c491d6d8fda9f815355033f54cb6aa
Jakob Jungreithmeir <jakob.jungreithmeir@bittex.at>
2018-06-28 15:32:15 +0200

    add specific versions to dependencies

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     version='1.0',
     py_modules=['cli'],
     install_requires=[
-        'argparse',
-        'PyJWT',
-        'requests',
-        'configparser',
-        'cryptography'
+        'PyJWT==1.6.4',
+        'requests==2.19.1',
+        'configparser==3.5.0',
+        'cryptography==2.2.2'
     ],
     entry_points='''
         [console_scripts]
